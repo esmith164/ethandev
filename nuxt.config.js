dd1b51b227840c03f8bf56b836aebbd9c82c3b1b
Ethan Smith <esmith164@mail.valenciacollege.edu>
2021-08-07 15:21:02 -0400

    tailwind setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,15 +7,20 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'ethandev',
+    title: 'Ethan - Software Developer from Orlando, FL',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400&family=Rubik:wght@500;600;700&display=swap' }
+    ],
+    script: [
+      { src: 'https://kit.fontawesome.com/a6f81dcb86.js' },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
@@ -41,5 +46,10 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
+    babel:{
+      plugins: [
+        ['@babel/plugin-proposal-private-methods', { loose: true }]
+      ]
+    }
   }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      'body': ['Inter', 'sans-serif'],
+      'display': ['Inter', 'sans-serif'],
+    },
+    colors: {
+      primary: '#2D4EF9',
+      white: '#FFFFFF',
+      light: '#959595',
+      dark: {
+        300: '#1E1D32',
+        400: '#121423'
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
